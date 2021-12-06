df764b9463f2936d623a866489aea7c420f1be80
adhityaf <adhityafebhiakbar@gmail.com>
2021-12-06 18:35:57 +0700

    Menambah Relationship, Prewaiting room, refactoring

--- app/Models/UserQuestionRoom.php
+++ app/Models/UserQuestionRoom.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Room;
+use App\Models\Question;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserQuestionRoom extends Model
 {
@@ -20,4 +22,19 @@ class UserQuestionRoom extends Model
         'password',
         'remember_token',
     ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
