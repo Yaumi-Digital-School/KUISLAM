7d515131b33e4194e9fac9c85a231b1357c2b477
ErkiKadhafi <erki.qaedhafi@gmail.com>
2021-12-11 10:19:50 +0700

    fix register button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,10 +34,10 @@
                                 <span class="ml-2 text-white text-md">Lanjutkan dengan Google</span>
                             </a>
                             <p class="text-center">atau</p>
-                            <button class="w-5/6 bg-green-lightBg mx-auto text-white rounded-md py-2 hover:bg-green-darkBg transition flex items-center space-x-1">
-                                <a href="{{ route('register_form') }}" class="flex items-center text-center  mx-auto"><svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 0H2C0.9 0 0.00999999 0.9 0.00999999 2L0 14C0 15.1 0.9 16 2 16H18C19.1 16 20 15.1 20 14V2C20 0.9 19.1 0 18 0ZM17 14H3C2.45 14 2 13.55 2 13V4L8.94 8.34C9.59 8.75 10.41 8.75 11.06 8.34L18 4V13C18 13.55 17.55 14 17 14ZM10 7L2 2H18L10 7Z" fill="white"/></svg><span class="ml-2">Lanjutkan dengan email</span> 
-                                </a>
-                            </button>
+                            <a href="{{ route('register_form') }}" class="flex justify-center items-center bg-green-lightBg hover:bg-green-darkBg w-5/6 mx-auto px-3 py-2 rounded-md transition">
+                                <x-mail-logo />
+                                <span class="ml-2 text-white text-md">Lanjutkan dengan Google</span>
+                            </a>
                             <div class="text-center"><span>Sudah punya akun?</span> <a class="text-green-lightBg" href="{{ route('login') }}">masuk</a></div>
                         </div>
                     </div>
